93fb7b3e5e37dcb4f002b66fdb1070540e7ce7cc
Önder Koşar <onderkosar@hotmail.com>
2020-08-22 17:11:39 +0300

    Initial Commit

---
+++ OKCovid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OKCovid
+//
+//  Created by Önder Koşar on 22.08.2020.
+//  Copyright © 2020 Önder Koşar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
